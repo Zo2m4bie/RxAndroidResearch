96bf07c752a9c1fe15e28eaacc46437a9eb8ebf4
Bubu_Kunz <yan6071992@gmail.com>
2017-04-11 08:17:59 +0300

    multithreading example done

--- app/src/yan/java/com/rxandroid/fragment/IoFragment.java
+++ app/src/yan/java/com/rxandroid/fragment/IoFragment.java
@@ -1,42 +1,27 @@
 package com.rxandroid.fragment;
 
-import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.rxandroid.DataManager;
-import com.rxandroid.JobExecutor;
 import com.rxandroid.R;
 
-import java.util.List;
-
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.schedulers.Schedulers;
-
 /**
- * Created by yzubritskiy on 4/9/2017.
+ *io(): Creates and returns a Scheduler intended for IO-bound work.
+ * The implementation is backed by an Executor thread-pool that will grow as needed.
+ *  This can be used for asynchronously performing blocking IO.
+ *  Do not perform computational work on this scheduler. Use Schedulers.computation() instead,
+ *  because io() is unbounded and if you schedule a thousand computational tasks on io() in parallel
+ *  then each of those thousand tasks will each have their own thread and be competing for CPU incurring context switching costs.
+ *  Use, for example, for interaction with the file system, interaction with databases or services on a different host
  */
 
 public class IoFragment extends Fragment {
 
-    private TextView mResultFlatMapTextView;
-    private TextView mResultConcatMapTextView;
-    private Button mFlatMapButton;
-    private Button mConcatMapButton;
-    private DataManager mDataManager;
+
 
     public IoFragment() {}
 
@@ -54,107 +39,14 @@ public class IoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_concatmap, container, false);
+        return inflater.inflate(R.layout.fragment_io, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mResultFlatMapTextView = (TextView) view.findViewById(R.id.result_faltmap_tv);
-        mResultConcatMapTextView = (TextView) view.findViewById(R.id.result_concatmap_tv);
-        mFlatMapButton = (Button) view.findViewById(R.id.do_flatmap_btn);
-        mConcatMapButton = (Button) view.findViewById(R.id.do_concatmap_btn);
-        view.findViewById(R.id.link).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uriUrl = Uri.parse("https://fernandocejas.com/2015/01/11/rxjava-observable-tranformation-concatmap-vs-flatmap/");
-                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                startActivity(launchBrowser);
-            }
-        });
-
-        mDataManager = new DataManager();
-        Log.d("TAG_", "onViewCreated");
-        JobExecutor executor = JobExecutor.getInstance();
-
-        mFlatMapButton.setOnClickListener(v -> doFlatMap(executor));
-        mConcatMapButton.setOnClickListener(v -> doConcatMap(executor));
-    }
-
-    private void doFlatMap(JobExecutor executor){
-        StringBuilder sourceBuilder = new StringBuilder("source -> ");
-        emit(mDataManager.getNumbers())
-                .flatMap(new Function<Integer, Observable<Integer>>() {
-                    @Override
-                    public Observable<Integer> apply(Integer integer) throws Exception {
-                        sourceBuilder.append(integer+", ");
-                        return Observable.just(integer*integer)
-                                 .subscribeOn(Schedulers.io());
-
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .toList()
-                .map(new Function<List<Integer>, String>() {
-                    @Override
-                    public String apply(List<Integer> integers) throws Exception {
-                        return listNumbersToString(integers);
-                    }
-                })
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String result) throws Exception {
-                        mResultFlatMapTextView.setText(sourceBuilder+"\n"+result);
-                    }
-                });
 
     }
 
-    private void doConcatMap(JobExecutor executor){
-        StringBuilder sourceBuilder = new StringBuilder("source -> ");
-        emit(mDataManager.getNumbers())
-                .concatMap(new Function<Integer, Observable<Integer>>() {
-                    @Override
-                    public Observable<Integer> apply(Integer integer) throws Exception {
-                        sourceBuilder.append(integer+", ");
-                        return Observable.just(integer*integer)
-                                .subscribeOn(Schedulers.io());
 
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .toList()
-                .map(new Function<List<Integer>, String>() {
-                    @Override
-                    public String apply(List<Integer> integers) throws Exception {
-                        return listNumbersToString(integers);
-                    }
-                })
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String result) throws Exception {
-                        mResultConcatMapTextView.setText(sourceBuilder+"\n"+result);
-                    }
-                });
-    }
-
-    private Observable<Integer> emit(List<Integer> integers){
-        return Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
-                for(Integer integer: integers){
-                    e.onNext(integer);
-                }
-                e.onComplete();
-            }
-        });
-    }
-
-    private String listNumbersToString(List<Integer> integers){
-        StringBuilder builder = new StringBuilder("result -> ");
-        for(Integer integer: integers){
-            builder.append(integer+", ");
-        }
-        return builder.toString();
-    }
 }
